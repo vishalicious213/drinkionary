2a6abf22939d8e62fe774558abacdd2475ec9a13
vishalicious213 <vishalicious213@gmail.com>
2024-10-29 18:49:25 -0700

    added event listener to #menu and event handler that looks at IDs in menu

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
 const menu = document.getElementById("menu")
 const renderSection = document.getElementById("render-area")
+
+// ⬇️ EVENT LISTENERS ⬇️
+
+menu.addEventListener("click", handleMenuClick)
+
+// ⬇️ EVENT HANDLERS ⬇️
+
+function handleMenuClick(e) {
+    console.log(e.target.id)
+
+    if (e.target.id === "cocktail-btn") {
+        console.log("Get cocktail info")
+    }
+}
+
+// ⬇️ UTILITY FUNCTIONS ⬇️
