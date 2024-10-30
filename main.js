cabb2ed1b081d82b297081a60bc538c9476392ba
vishalicious213 <vishalicious213@gmail.com>
2024-10-29 19:05:08 -0700

    getting cocktail data via getData() and logging it via renderCocktails()

--- main.js
+++ main.js
@@ -11,8 +11,39 @@ function handleMenuClick(e) {
     console.log(e.target.id)
 
     if (e.target.id === "cocktail-btn") {
-        console.log("Get cocktail info")
+        getData("s")
     }
 }
 
 // ⬇️ UTILITY FUNCTIONS ⬇️
+
+function getData(btn) {
+    let searchTerm = ""
+
+    if (btn === "s") {
+        searchTerm = document.getElementById("search-cocktail").value
+    }
+
+    if (!searchTerm) {
+        return
+    }
+
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?${btn}=${searchTerm}`)
+    .then(res => res.json())
+    .then(data => {
+        // console.log(data)
+
+        if (btn === "s") {
+            renderCocktails(data.drinks)
+        }
+    })
+    .catch(err => {
+        console.log(`Error: ${err}`)
+    })
+}
+
+// ⬇️ RENDER FUNCTIONS ⬇️
+
+function renderCocktails(arr) {
+    console.log(arr)
+}
